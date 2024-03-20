78908f1f542c199a59b107bf4e0ce9a39abbd092
lisandrasilva <lisandrasilva18@hotmail.com>
2024-03-20 09:03:31 +0000

    Added freshAddress() cheatcode

--- src/KontrolCheats.sol
+++ src/KontrolCheats.sol
@@ -31,6 +31,10 @@ interface KontrolCheatsBase {
     function freshUInt(uint8) external returns (uint256);
     // Returns a symbolic boolean value
     function freshBool() external returns (uint256);
+    // Returns a symbolic byte array
+    function freshBytes(uint256) external returns (bytes memory);
+    // Returns a symbolic address value
+    function freshAddress() external returns (address);
 }
 
 abstract contract KontrolCheats {
