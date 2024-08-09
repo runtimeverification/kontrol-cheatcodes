9e245aa3da16ec4cecd049d641e9b99af5b78aae
Petar Maksimovic <petar.maksimovic@runtimeverification.com>
2024-08-09 13:13:01 +0200

    copyStorage cheatcode

--- src/IKontrolCheatsBase.sol
+++ src/IKontrolCheatsBase.sol
@@ -19,7 +19,9 @@ interface KontrolCheatsBase {
     function expectCreate2(address,uint256,bytes calldata) external;
     // Makes the storage of the given address completely symbolic.
     function symbolicStorage(address) external;
-    // From now on, whenever a call is made to callee with calldata data, instead call calledContract with the same calldata. 
+    // Copies the storage of one account into another
+    function copyStorage(address,address) external;
+    // From now on, whenever a call is made to callee with calldata data, instead call calledContract with the same calldata.
     function mockFunction(address callee, address calledContract, bytes calldata data) external;
     // Adds an address to the whitelist.
     function allowCallsToAddress(address) external;
